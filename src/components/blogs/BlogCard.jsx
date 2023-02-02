ab8ca309d5a95ec19459049bbcb1a8a9f1c88b85
Ahmad Software <dr.ahmad.salah.54@gmail.com>
2023-02-02 20:42:04 +0300

    fix blog card height and flex

--- src/components/blogs/BlogCard.jsx
+++ src/components/blogs/BlogCard.jsx
@@ -4,22 +4,26 @@ import { Link } from "react-router-dom";
 import DOMPurify from "dompurify";
 const BlogCard = ({ time, id, img, enBody, arBody, krBody, BACKEND_HOST, t, i18n }) => {
   return (
-    <div className="blogCard flex flex-column justify-left align-center  gap-1">
-      <img src={`${img}`} alt="blogImage" />
-      {i18n.language === "en" ? (
-        <p>{DOMPurify.sanitize(enBody.replace(/<br\s*[\/]?>/gi, ""), { ADD_TAGS: ["br"] }).substring(0, 120) + "..."}</p>
-      ) : i18n.language === "ar" ? (
-        <p>{DOMPurify.sanitize(arBody.replace(/<br\s*[\/]?>/gi, ""), { ADD_TAGS: ["br"] }).substring(0, 120) + "..."}</p>
-      ) : (
-        <p>{DOMPurify.sanitize(krBody.replace(/<br\s*[\/]?>/gi, ""), { ADD_TAGS: ["br"] }).substring(0, 120) + "..."}</p>
-      )}
-      <span className="blogTime">
-        {moment(time).format("MMMM Do YYYY")} {moment(time).format("dddd")}
-      </span>
+    <div className="blogCard flex flex-column justify-between align-center  gap-1">
+      <div className="flex flex-column justify-center align-center gap-1">
+        <img src={`${img}`} alt="blogImage" />
+        {i18n.language === "en" ? (
+          <p>{DOMPurify.sanitize(enBody.replace(/<br\s*[\/]?>/gi, ""), { ADD_TAGS: ["br"] }).substring(0, 120) + "..."}</p>
+        ) : i18n.language === "ar" ? (
+          <p>{DOMPurify.sanitize(arBody.replace(/<br\s*[\/]?>/gi, ""), { ADD_TAGS: ["br"] }).substring(0, 120) + "..."}</p>
+        ) : (
+          <p>{DOMPurify.sanitize(krBody.replace(/<br\s*[\/]?>/gi, ""), { ADD_TAGS: ["br"] }).substring(0, 120) + "..."}</p>
+        )}
+      </div>
+      <div className="flex flex-column justify-center align-center gap-1">
+        <span className="blogTime">
+          {moment(time).format("MMMM Do YYYY")} {moment(time).format("dddd")}
+        </span>
 
-      <Link to={`/blogs/${id}`} className="readBlog">
-        {t("blog.read")}
-      </Link>
+        <Link to={`/blogs/${id}`} className="readBlog">
+          {t("blog.read")}
+        </Link>
+      </div>
     </div>
   );
 };
--- src/components/blogs/BlogPageCard.jsx
+++ src/components/blogs/BlogPageCard.jsx
@@ -5,21 +5,26 @@ import DOMPurify from "dompurify";
 
 const BlogPageCard = ({ time, img, enBody, arBody, krBody, id, BACKEND_HOST, t, i18n }) => {
   return (
-    <div className="blogCard flex flex-column justify-left align-center  gap-1">
-      <img src={`${img}`} alt="blogImage" />
-      {i18n.language === "en" ? (
-        <p>{DOMPurify.sanitize(enBody.replace(/<br\s*[\/]?>/gi, ""), { ADD_TAGS: ["br"] }).substring(0, 120) + "..."}</p>
-      ) : i18n.language === "ar" ? (
-        <p>{DOMPurify.sanitize(arBody.replace(/<br\s*[\/]?>/gi, ""), { ADD_TAGS: ["br"] }).substring(0, 120) + "..."}</p>
-      ) : (
-        <p>{DOMPurify.sanitize(krBody.replace(/<br\s*[\/]?>/gi, ""), { ADD_TAGS: ["br"] }).substring(0, 120) + "..."}</p>
-      )}
-      <span className="blogTime">
-        {moment(time).format("MMMM Do YYYY")} {moment(time).format("dddd")}
-      </span>
-      <Link to={`${id}`} className="readBlog">
-        {t("blog.read")}
-      </Link>
+    <div className="blogCard flex flex-column justify-between align-center  gap-1">
+      <div className="flex flex-column justify-center align-center gap-1">
+        <img src={`${img}`} alt="blogImage" />
+        {i18n.language === "en" ? (
+          <p>{DOMPurify.sanitize(enBody.replace(/<br\s*[\/]?>/gi, ""), { ADD_TAGS: ["br"] }).substring(0, 120) + "..."}</p>
+        ) : i18n.language === "ar" ? (
+          <p>{DOMPurify.sanitize(arBody.replace(/<br\s*[\/]?>/gi, ""), { ADD_TAGS: ["br"] }).substring(0, 120) + "..."}</p>
+        ) : (
+          <p>{DOMPurify.sanitize(krBody.replace(/<br\s*[\/]?>/gi, ""), { ADD_TAGS: ["br"] }).substring(0, 120) + "..."}</p>
+        )}
+      </div>
+      <div className="flex flex-column justify-center align-center gap-1">
+        <span className="blogTime">
+          {moment(time).format("MMMM Do YYYY")} {moment(time).format("dddd")}
+        </span>
+
+        <Link to={`/blogs/${id}`} className="readBlog">
+          {t("blog.read")}
+        </Link>
+      </div>
     </div>
   );
 };
