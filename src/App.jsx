b3738f07b2c05dff02e860e220716350e9788ece
Ahmad Software <dr.ahmad.salah.54@gmail.com>
2023-09-20 23:19:56 +0300

    change cv

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,48 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Fallback from "./components/Fallback.jsx";
 const UpdateBlog = lazy(() => import("./pages/admin/UpdateBlog.jsx"));
 const CreateBlog = lazy(() => import("./pages/admin/CreateBlog.jsx"));
 const Landing = lazy(() => import("./pages/landing/Landing.jsx"));
-const Layout = lazy(() => import("./components/layout/Layout"));
 const Blogs = lazy(() => import("./pages/blogs/Blogs"));
-const ErrorPage = lazy(() => import("./error/ErrorPage"));
-const Error = lazy(() => import("./error/Error"));
 const SingleBlogPage = lazy(() => import("./pages/singleBlog/SingleBlogPage"));
-const Universal = lazy(() => import("./components/Universal"));
 const Panel = lazy(() => import("./pages/panel/Panel"));
 const Login = lazy(() => import("./pages/Login"));
 const AdminRoutes = lazy(() => import("./routes/AdminRoutes"));
 const Admins = lazy(() => import("./pages/admin/Admins"));
+import ErrorPage from "./error/ErrorPage";
+import Error from "./error/Error.jsx";
+import Layout from "./components/layout/Layout.jsx";
+import Universal from "./components/Universal.jsx";
+
 const routes = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Universal />}>
       {/* home route */}
-      <Route path="/" element={<Layout isHome={true} />} errorElement={<Error />}>
+      <Route
+        path="/"
+        element={<Layout isHome={true} />}
+        errorElement={<Error />}
+      >
         <Route index element={<Landing />} errorElement={<Error />} />
       </Route>
       {/* blogs routes */}
-      <Route path="/blogs" element={<Layout isHome={false} />} errorElement={<Error />}>
+      <Route
+        path="/blogs"
+        element={<Layout isHome={false} />}
+        errorElement={<Error />}
+      >
         {/* single blog  route */}
         <Route index element={<Blogs />} errorElement={<Error />} />
-        <Route path=":id" element={<SingleBlogPage errorElement={<Error />} />} />
+        <Route
+          path=":id"
+          element={<SingleBlogPage errorElement={<Error />} />}
+        />
       </Route>
       <Route path="/login" element={<Login />} errorElement={<Error />} />
       <Route element={<AdminRoutes layout={true} />} errorElement={<Error />}>
@@ -33,8 +50,16 @@ const routes = createBrowserRouter(
         <Route path="/admins" element={<Admins />} errorElement={<Error />} />
       </Route>
       <Route element={<AdminRoutes layout={false} />} errorElement={<Error />}>
-        <Route path="/panel/create_blog" element={<CreateBlog />} errorElement={<Error />} />
-        <Route path="/panel/update_blog/:blog_id" element={<UpdateBlog />} errorElement={<Error />} />
+        <Route
+          path="/panel/create_blog"
+          element={<CreateBlog />}
+          errorElement={<Error />}
+        />
+        <Route
+          path="/panel/update_blog/:blog_id"
+          element={<UpdateBlog />}
+          errorElement={<Error />}
+        />
       </Route>
 
       {/* page 404 route */}
