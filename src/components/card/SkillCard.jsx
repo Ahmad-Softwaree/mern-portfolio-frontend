e5ec900c9f57388a9636cf9543a433ae17ff7b6a
Ahmad Software <dr.ahmad.salah.54@gmail.com>
2024-06-12 18:12:48 +0300

    fix skill card images height

--- src/components/card/SkillCard.jsx
+++ src/components/card/SkillCard.jsx
@@ -2,7 +2,7 @@ export default function SkillCard({ val }) {
   return (
     <div className="w-fit max-w-fit rounded-md cursor-pointer p-1 px-2 transition-all flex  flex-row justify-left  items-center gap-2 border-white-500/[0.2]  duration-300 bg-black-600 shadow-xl  flex-auto border-2 border-solid">
       <img
-        className="w-[25px] object-cover rounded-full"
+        className="w-[25px] h-[25px] object-cover rounded-full"
         src={val.image}
         alt="skillImage"
       />
